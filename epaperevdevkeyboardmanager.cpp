rmitchellscott/epaper-qpa

commit f8d6683a0aa8ffbe8c2072ddd947daed95053ace

fix: register hall sensor as keyboard
Task: CT-5072

epaperevdevkeyboardmanager.cpp
@@ -101,6 +101,9 @@ EpaperEvdevKeyboardManager::EpaperEvdevKeyboardManager(const QString &key, const
     for (const QString &device : qAsConst(parsed.devices))
         addKeyboard(device);
 
+    // add hall sensor
+    addKeyboard("/dev/input/event1");
+
     if (parsed.devices.isEmpty()) {
         qCDebug(qLcEvdevKey, "evdevkeyboard: Using device discovery");
         if (auto deviceDiscovery = QDeviceDiscovery::create(QDeviceDiscovery::Device_Keyboard, this)) {

commit a9f1c036e2103b6e9372b7d394bc44484f47ff9c

EpaperEvdevKeyboardManager: Fix hall sensors path
Use hardware agnostic path to the gpio hall sensors event.
This fixes an issue were the wrong input event was added,
and thus was triggered twice.

Fixes: f8d6683 ("fix: register hall sensor as keyboard")

epaperevdevkeyboardmanager.cpp
@@ -69,7 +69,7 @@ EpaperEvdevKeyboardManager::EpaperEvdevKeyboardManager(const QString &key, const
         addKeyboard(device);
 
     // add hall sensor
-    addKeyboard("/dev/input/event1");
+    addKeyboard("/dev/input/by-path/platform-gpio-hall-sensors-event");
 
     if (parsed.devices.isEmpty()) {
         qCDebug(qLcEvdevKey, "evdevkeyboard: Using device discovery");